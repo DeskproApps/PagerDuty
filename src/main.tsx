3e020bd3f4e46a70a2133d01ac38246d42001cbc
ilia makarov <ilia.makarov@me.com>
2024-02-23 12:08:42 +0200

    updated deps

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,13 @@ import "./index.css";
 import App from "./App";
 import { DeskproAppProvider } from "@deskpro/app-sdk";
 import "iframe-resizer/js/iframeResizer.contentWindow.js";
-import { lightTheme } from "@deskpro/deskpro-ui";
-import { ThemeProvider } from "styled-components";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 
 root.render(
   <React.StrictMode>
     <DeskproAppProvider>
-      <ThemeProvider theme={lightTheme}>
-        <App />
-      </ThemeProvider>
+      <App />
     </DeskproAppProvider>
   </React.StrictMode>
 );
