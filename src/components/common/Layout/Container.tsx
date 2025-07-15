f2b4f23ffd263ea7b804cda8f672ca6d0fe0ccb1
Paul <1740195+HappyPaul55@users.noreply.github.com>
2025-07-15 17:25:28 +0100

    [Chore] Upgrade deps 2025-07 (#56)

--- src/components/common/Layout/Container.tsx
+++ src/components/common/Layout/Container.tsx
@@ -1,6 +1,7 @@
+import type { DeskproAppTheme } from "@deskpro/app-sdk";
 import styled from "styled-components";
 
-const Container = styled.div`
+const Container = styled.div<DeskproAppTheme>`
   min-height: 2em;
   margin: 8px;
 `;
